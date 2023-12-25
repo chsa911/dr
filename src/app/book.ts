b7fe387cbbb89dbc3f51f14f05948bfb7f9e9a58
christopher sanders <ch.sa911@gmail.com>
2023-12-25 13:09:44 +0100

    Implementation of books.component without showing

---
+++ src/app/book.ts
@@ -0,0 +1,10 @@
+  export interface book{
+      id: number;
+      inputDate: Date;
+      authorLastName: string;
+      keyWord: string;
+      position: number;
+      publisher: string;
+      pages: number;
+      endDate: Date;
+  }
